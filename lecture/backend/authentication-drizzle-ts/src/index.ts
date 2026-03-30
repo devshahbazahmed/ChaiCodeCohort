cfa654bef67b3fe9766cc2491be0387ff36f5240
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-03-30 12:08:22 +0530

    Basic Express Setup

--- lecture/backend/authentication-drizzle-ts/src/index.ts
+++ lecture/backend/authentication-drizzle-ts/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello");
+import { createServer } from "node:http";
+import { createApplication } from "./app/index.js";
+
+async function main() {
+  try {
+    const server = createServer(createApplication());
+    const PORT: number = 8080;
+
+    server.listen(PORT, () =>
+      console.log(`HTTP server is running on port ${PORT}`)
+    );
+  } catch (error) {
+    console.error(`Error starting http server`);
+    throw error;
+  }
+}
+
+main();
