07cfe9c77a0bb185de0855d3309cb58cc6ee16ca
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-03-30 19:51:26 +0530

    Signup route

---
+++ lecture/backend/authentication-drizzle-ts/src/app/auth/routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import type { Router } from "express";
+import AuthenticationController from "./controller";
+
+const authenticationController = new AuthenticationController();
+
+export const authRouter: Router = express.Router();
+
+authRouter.post(
+  "/signup",
+  authenticationController.handleSignup.bind(authenticationController)
+);
--- lecture/backend/authentication-drizzle-ts/src/app/index.ts
+++ lecture/backend/authentication-drizzle-ts/src/app/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import type { Express } from "express";
 
+import { authRouter } from "./auth/routes";
+
 export function createApplication(): Express {
   const app = express();
+
+  app.use(express.json());
+
+  app.use("/auth", authRouter);
 
   app.get("/", (req, res) => {
     return res.json({ message: "Welcome to ChaiCode auth service" });
