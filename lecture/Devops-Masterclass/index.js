e4ce10cb00a78d9e823733227487a52d74a8eae9
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-06-07 23:13:12 +0530

    Add deployment workflow and update server health status

--- lecture/Devops-Masterclass/index.js
+++ lecture/Devops-Masterclass/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT ?? 8000;
 app.get('/', (req, res) => {
   return res.json({
     message: 'Server is healthy and running v2',
-    status: 'good',
+    status: 'Github Actions',
   });
 });
 
