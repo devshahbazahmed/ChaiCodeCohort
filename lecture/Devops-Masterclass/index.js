db2aa8c384d886f4d240bcc36c49b7d55cda333c
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-06-07 19:43:55 +0530

    Update server health message to indicate version 2

--- lecture/Devops-Masterclass/index.js
+++ lecture/Devops-Masterclass/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Server is healthy and running' });
+  return res.json({ message: 'Server is healthy and running v2' });
 });
 
 app.listen(PORT, () => {
